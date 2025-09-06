dfe44093915705fcd453036f0d692da053f809c0
Dai <nguyenngocdai2802@gmail.com>
2025-09-06 15:48:12 +1200

    misc

--- Assets/Scripts/Inventory/ItemDefinition.cs
+++ Assets/Scripts/Inventory/ItemDefinition.cs
@@ -7,6 +7,8 @@ public class ItemDefinition : ScriptableObject
     public string ID = Guid.NewGuid().ToString();
     public string FriendlyName;
     public string Description;
+    public string affectedStat;
+    public int effectValue;
     public Sprite Icon;
     public Dimensions SlotDimension;
 }
