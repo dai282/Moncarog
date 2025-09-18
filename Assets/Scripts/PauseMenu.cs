968973a652e94b6820bb9ab57bd0928ba9497d6a
Joseph Nicholls <finkarush@gmail.com>
2025-09-18 14:20:53 +1200

    Pause Menu Main Scene Integration

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    [Header("References")]
     public GameObject pauseMenu;
+    public GameObject pauseButton;
     public bool isPaused;
 
     void Start()
@@ -10,32 +12,21 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
-            {
-                ResumeGame();
-            }
-            else
-            {
-                PauseGame();
-            }
-        }
-    }
-
+    // Called by a UI Button
     public void PauseGame()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+        pauseButton.SetActive(false);
     }
 
+    // Called by a UI Button
     public void ResumeGame()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+        pauseButton.SetActive(true);
     }
 }
