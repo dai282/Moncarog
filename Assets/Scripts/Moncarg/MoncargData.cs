1cecf3321f399930eea90555afda2c2137c12b4f
NikoGoijarts <jnc6209@autuni.ac.nz>
2025-09-22 11:49:58 +1200

    created boss and minboss booleans in moncarg data
    currently skeltis is a boss, grassmancer is a miniboss, aquaster is a miniboss, firezard is aminiboss.

--- Assets/Scripts/Moncarg/MoncargData.cs
+++ Assets/Scripts/Moncarg/MoncargData.cs
@@ -19,6 +19,8 @@ public class MoncargData
     public ElementalType type;
     public bool active;
     public SkillDefinition[] skillset = new SkillDefinition[4];
+    public bool isBoss;
+    public bool isMiniBoss;
 
     public void reset()
     {
