a5325dede2354d3f963f31dc54732ee898219871
light304 <coopergardyne2@gmail.com>
2025-10-23 12:55:06 +1300

    Minor adjustment w config

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -40,7 +40,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InitializeGame();
+        //InitializeGame();
     }
     
     public void Game()
